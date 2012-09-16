37fe615ae1cbca441e9d5ff2c2179914875251f7
Frederico Araujo <fred.the.master@gmail.com>
2012-09-16 23:07:46 +0700

    updated jar files

--- lib/trinidad_scheduler_extension/jars.rb
+++ lib/trinidad_scheduler_extension/jars.rb
@@ -1,4 +1,4 @@
-require 'trinidad_scheduler_extension/jars/log4j-1.2.16'
-require "trinidad_scheduler_extension/jars/slf4j-api-1.6.1"
-require "trinidad_scheduler_extension/jars/slf4j-log4j12-1.6.1"
-require "trinidad_scheduler_extension/jars/quartz-1.8.4"
+require 'trinidad_scheduler_extension/jars/log4j-1.2.17'
+require "trinidad_scheduler_extension/jars/slf4j-api-1.7.1"
+require "trinidad_scheduler_extension/jars/slf4j-log4j12-1.7.1"
+require "trinidad_scheduler_extension/jars/quartz-2.1.6"
